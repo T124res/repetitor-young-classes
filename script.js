92be06bcc58215f0442ccc6f1f4df560e7a6eda9
T124res <kovtibb@gmail.com>
2025-01-10 14:28:02 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-// Кнопка для повернення на головну
-function scrollToHome() {
-    document.getElementById('home').scrollIntoView({ behavior: 'smooth' });
-}
 
-// Обробка форми
-document.getElementById('contact-form').addEventListener('submit', function(event) {
-    event.preventDefault(); // Забороняємо стандартну відправку
-    alert('Дякуємо за вашу заявку!');
-});
